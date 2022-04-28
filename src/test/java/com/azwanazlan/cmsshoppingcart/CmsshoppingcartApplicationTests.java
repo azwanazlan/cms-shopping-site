affe09f8cc07ffae62035c94531aea534d985ce2
Azwan Azlan <azwanazlan2809@gmail.com>
2022-04-28 15:58:03 +0800

    Initial commit

---
+++ src/test/java/com/azwanazlan/cmsshoppingcart/CmsshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azwanazlan.cmsshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsshoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
