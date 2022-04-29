e558e3072e2b441d4c15f9f59e4877a1190e5acc
Azwan Azlan <azwanazlan2809@gmail.com>
2022-04-30 04:35:53 +0800

    Add Delete Page

---
+++ src/main/java/com/azwanazlan/cmsshoppingcart/models/PageRepository.java
@@ -0,0 +1,19 @@
+package com.azwanazlan.cmsshoppingcart.models;
+
+import java.util.List;
+
+import com.azwanazlan.cmsshoppingcart.models.data.Page;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ * PageRepository
+ */
+public interface PageRepository extends JpaRepository<Page, Integer>{
+
+   @Override
+   List<Page> findAll();
+
+    
+}
